ad8c9cdb0a2b0bd377fd7a9305712c0e7538f920
Leandro Ferretti <seleandroferretti@gmail.com>
2021-05-07 19:12:51 -0300

    first simple react native app commit

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
 
-export default function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  // abrir el picker al tocar la imagen inicial y seleccionar imagen
+  let openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to camara roll is required");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  // abrir el panel de aplicaciones para compartir al tocar el boton de compartir
+  let openShareDialog = async () => {
+    // verificando si el dispositivo puede compartir imagenes
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(
+        `The image share is available for sharing at: ${selectedImage.remoteUri}`
+      );
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Seleccione una imagen:</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://i.ibb.co/1LN0vsy/share.png",
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      {selectedImage ? (
+        <TouchableOpacity onPress={openShareDialog} style={styles.button}>
+          <Text style={styles.textoboton}>Compartir Imagen</Text>
+        </TouchableOpacity>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  titulo: {
+    color: "#61DBFB",
+    fontSize: 25,
+    marginBottom: 30,
+    fontWeight: "bold",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 200,
+    width: 200,
+    resizeMode: "contain",
+  },
+  button: { backgroundColor: "#61DBFB", padding: 7, marginTop: 30 },
+  textoboton: { fontSize: 20, color: "#fff" },
+  thumbnail: {
+    width: 130,
+    height: 130,
+    resizeMode: "contain",
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple React Native App
+
+A simple application to take photos from the gallery and share it through the different applications that mobile phones have, developed with React Native, expo-image-picker and expo-sharing.
